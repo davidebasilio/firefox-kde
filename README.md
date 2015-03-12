c542af935019234b54814559da9f29864b452c0c
Davide Basilio Bartolini <davideb@ethz.ch>
2015-03-12 13:29:45 +0100

    Script and .desktop file, first version

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # firefox-kde
-Wrapper to pair Firefox profiles with KDE sessions
+Wrapper to pair Firefox profiles with KDE sessions.
+I got the idea from here: https://bbs.archlinux.org/viewtopic.php?id=137941 and made it work with recent KDE and Firefox.
+
+Working for me on KDE 4.14 and Firefox 36.0.1.
+
+Just use the shelal script and .desktop file (you might have to adjust the script path there) as you default browser.
+
---
+++ firefox-kde.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+ACTIVITY=$(qdbus org.kde.kactivitymanagerd /ActivityManager/Activities CurrentActivity)
+FF_FOLDER="$HOME/.mozilla/firefox"
+PROFDIR="$FF_FOLDER/$ACTIVITY"
+
+if [ -d $FF_FOLDER/$ACTIVITY ]; then
+	echo "loading profile $ACTIVITY"
+else
+	echo "creating new profile for $ACTIVITY"
+        echo firefox -CreateProfile "$ACTIVITY $PROFDIR"
+	echo $?
+fi
+
+while [ ! -d $FF_FOLDER/$ACTIVITY ]; do
+	sleep 0.1
+done
+
+url="http://www.google.com#q=$1"
+if [[ "$1" == "http://"* ]]; then
+	url=$1
+elif [[ "$1" == "https://"* ]]; then
+	url=$1
+elif [[ "$1" == "www."* ]]; then
+	url="http://$1"
+elif [[ "$1" == "/"* ]]; then
+	url="file://$1"
+fi
+
+firefox -P $ACTIVITY -remote "ping()" &> /dev/null
+p=$?
+if [ $p == 0 ]; then # profile already open
+	# kdialog --msgbox "/usr/bin/firefox -P $ACTIVITY -remote \"openURL($url,new-tab)\""
+	/usr/bin/firefox -P $ACTIVITY -remote "openURL($url,new-tab)" &> /tmp/firefox-kde.log
+else # open new instance
+	# kdialog --msgbox "/usr/bin/firefox -P $ACTIVITY -new-instance $url"
+	/usr/bin/firefox -P $ACTIVITY -new-instance $ur &> /tmp/firefox-kde.logl
+fi
+
